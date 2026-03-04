d5bd3c6bf41c760048c7bde775cb3c0236ee4109
Utkarsh Yadav <utkarshyadav3078@gmail.com>
2026-03-04 20:03:11 +0530

    UI and function changes
    A simple JS code and fresh new UI

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
-let btn = document.querySelector("#btn");
-let inp = document.querySelector("input");
-let ul = document.querySelector("ul");
+let createButtonRef = document.getElementById("createButton");
+let userInputRef = document.getElementById("userInput");
+let todoListRef = document.getElementById("todoList");
 
-btn.addEventListener("click", function(){
-    let item = document.createElement("li");
-    item.innerText = inp.value;
-
-    let delbtn = document.createElement("button");
-    delbtn.innerText = "delete";
-    delbtn.classList.add("delete");
-
-    item.appendChild(delbtn);
-    ul.appendChild(item);
-    console.log(inp.value);
-    inp.value = "";
-})
-
-ul.addEventListener("click",function(event){
-    // console.dir(event.target.nodeName);
-    if(event.target.nodeName == "BUTTON"){
-        let listItem = event.target.parentElement;
-        listItem.remove();
-        console.log("deleted successfully");
-    }
-})
-
-// let delbtns=document.querySelectorAll(".delete");
-// for(delBtn of delbtns){
-//     delBtn.addEventListener("click", function(){
-//         let par = this.parentElement;
-//         par.remove();
-//     })
-// }
+createButtonRef.addEventListener("click", function () {
+  const value = userInputRef.value;
+  //Validation for empty user input
+  if (value === "") {
+    alert("Please write the task");
+    return;
+  }
+  const li = document.createElement("li");
+  li.innerText = value;
+  //
+  li.addEventListener("click", function () {
+    li.remove();
+  });
+  todoListRef.appendChild(li);
+  userInputRef.value = "";
+});
